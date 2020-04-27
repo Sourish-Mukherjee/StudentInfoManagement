f674d4ce28c4c1c1cc01cca110ea826adb3667c5
Sourish Mukherjee <mukherjee.sourish09@gmail.com>
2020-04-27 17:57:05 +0530

    Inital Commit

---
+++ src/Information/Student.java
@@ -0,0 +1,77 @@
+package Information;
+
+
+public class Student {
+
+    private String name, usn, sec, branch;
+    private int year, sem;
+
+    public Student() {
+        this("", "", "", "", 0, 0);
+    }
+
+    public Student(String name, String usn, String branch, String sec, int year, int sem) {
+        this.name = name;
+        this.usn = usn;
+        this.sec = sec;
+        this.year = year;
+        this.sem = sem;
+        this.branch = branch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsn() {
+        return usn;
+    }
+
+    public void setUsn(String usn) {
+        this.usn = usn;
+    }
+
+    public String getSec() {
+        return sec;
+    }
+
+    public void setSec(String sec) {
+        this.sec = sec;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSem() {
+        return sem;
+    }
+
+    public void setSem(int sem) {
+        this.sem = sem;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    @Override
+    public String toString() {
+        return ("Name : " + this.getName() + " USN : " + this.getUsn()
+                + " Year: " + this.getYear() + System.lineSeparator() + "Branch : " + this.getBranch()
+                + " Semester : " + this.getSem()+" Section : "+this.getSec());
+    }
+
+}
---
+++ src/Main/MainClass.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Information.Student;
+
+public class MainClass {
+    
+    public static void main(String args[])
+    {
+        Student st = new Student("Suhas", "1CR18CS162", "CSE", "C", 2, 4);
+        System.out.println(st);
+    }
+    
+}
