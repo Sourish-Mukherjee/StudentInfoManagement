f0eea609da156aac57e20ba97694548b38bc11b1
Sourish Mukherjee <mukherjee.sourish09@gmail.com>
2020-06-03 03:05:57 +0530

    Added Email,Name,USN for StudentDashboardController

--- src/dashboard/StudentDashboardController.java
+++ src/dashboard/StudentDashboardController.java
@@ -3,14 +3,43 @@
 package dashboard;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.sql.ResultSet;
+
+import authentication.LoginController;
+import authentication.RegisterFXMLController;
+import database.DataBaseHelper;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class StudentDashboardController implements Initializable {
 
+    @FXML
+    private Label stuDashName,
+            stuDashUSN,
+            stuDashEmail;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //TO-DO Something
-    }    
-    
+        String loggedEmail = LoginController.getEmaiL();
+        DataBaseHelper db = new DataBaseHelper();
+        try {
+            int id = new RegisterFXMLController().findID(db, loggedEmail);
+            ResultSet set = db.getStatement().executeQuery("Select name,usn from studententry" +
+                    " where link_id = " + id+";");
+            if (set.next()) {
+                String name = set.getString(1);
+                String usn = set.getString(2);
+                stuDashName.setText(name);
+                stuDashUSN.setText(usn);
+            } else
+                throw new SQLException("ResultSet = false in initialize method of Student DashBoard");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        stuDashEmail.setText(loggedEmail);
+    }
+
 }
