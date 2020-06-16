d60a3e1c2cb4c5cdfb08e7e85e89221798962289
Sourish-Mukherjee <mukherjee.sourish09@gmail.com>
2020-06-17 01:51:22 +0530

    Fixed All Codacy Issues

--- src/dashboard/TeacherDashboardFXML.java
+++ src/dashboard/TeacherDashboardFXML.java
@@ -1,8 +1,6 @@
 package dashboard;
 
 import authentication.LoginController;
-import authentication.RegisterFXMLController;
-import database.DataBaseHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,9 +13,7 @@ import javafx.stage.Stage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class TeacherDashboardFXML implements Initializable {
